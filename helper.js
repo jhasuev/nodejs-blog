667d3026c6a7b22818aae9c891842d4708ab2df4
Jamal Hasuev <metafra@mail.ru>
2021-07-26 03:46:30 +0300

    add login logic

--- helper.js
+++ helper.js
@@ -3,10 +3,10 @@ const config = require("./config")
 module.exports = {
   checkValid: (type, value) => {
     if (config.form[type].minLength && config.form[type].minLength > value.length) {
-      return `Поле должен быть не менее ${config.form[type].minLength} символов`
+      return `Поле должно иметь не менее ${config.form[type].minLength} символов`
     }
     if (config.form[type].maxLength && config.form[type].maxLength < value.length) {
-      return `Поле должен быть не более ${config.form[type].maxLength} символов`
+      return `Поле должно иметь не более ${config.form[type].maxLength} символов`
     }
     return true
   },
--- router/routes/login.js
+++ router/routes/login.js
@@ -1,5 +1,42 @@
+const User = require("../../models/User")
+const { checkValid } = require("../../helper")
+
 module.exports = router => {
   router.get("/login", (req, res) => {
     res.render("login")
+  })
+
+  router.post("/login", async (req, res) => {
+    const login = req.body.login.trim()
+    const password = req.body.password.trim()
+    const errors = {}
+    let user = null
+    let valid = null
+
+    valid = checkValid("password", password)
+    if (valid !== true) {
+      errors.password = valid
+    }
+
+    valid = checkValid("login", login)
+    if (valid !== true) {
+      errors.login = valid
+    }
+    
+    if (!Object.keys(errors).length) {
+      user = await User.findOne({ login })
+      if (!user) {
+        errors.login = "Такого пользователя не существует"
+      } else if (user.password !== password) {
+        errors.password = "Пароль неверный"
+      }
+    }
+
+    if (Object.keys(errors).length) {
+      res.render("login", { errors, fields: { login, password } })
+    } else {
+      // TODO: сохранить _id в куки/сессии
+      res.redirect("/profile")
+    }
   })
 }
