437bff4e01586573a2329f65be69388c30827341
Jamal Hasuev <metafra@mail.ru>
2021-07-29 01:04:05 +0300

    add logic for showing single post

--- router/routes/post.js
+++ router/routes/post.js
@@ -1,5 +1,20 @@
+const Category = require("../../models/Category")
+const Post = require("../../models/Post")
+const Comment = require("../../models/Comment")
+
 module.exports = router => {
-  router.get("/post", (req, res) => {
-    res.render("post")
+  router.get("/post/:_id", async (req, res) => {
+    const post = await Post.findById(req.params._id).lean()
+    if (!post) return res.redirect("/")
+
+    post.category = await Category.findById(post.categoryId).lean()
+    post.commentsCount = await Comment.count({ postId: post._id })
+
+    // увеличиваем счетчик просмотра
+    await Post.findByIdAndUpdate(post._id, { views: post.views + 1 })
+
+    res.render("post", {
+      post,
+    })
   })
 }
