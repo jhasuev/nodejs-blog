8d7b7434b270c18b01dd16196f9c594db2ce8996
Jamal Hasuev <metafra@mail.ru>
2021-07-29 00:47:04 +0300

    show posts on index page

--- router/routes/index.js
+++ router/routes/index.js
@@ -1,5 +1,48 @@
+const Post = require("../../models/Post")
+const Comment = require("../../models/Comment")
+const Category = require("../../classes/Category")
+const config = require("../../config")
+const { getPagination } = require("../../helpers/index")
+
 module.exports = router => {
-  router.get("/", (req, res) => {
-    res.render("index")
+  router.get(["/", "/category/:category"], async (req, res) => {
+    const findParams = {}
+
+    // категории
+    const categories = await Category.getAllCategories(true)
+    const categorySlug = String(req.params.category).trim()
+    let categoryId = null
+    
+    if (categorySlug) {
+      categoryId = await Category.getCategory({ slug: categorySlug })
+      if (categoryId) findParams.categoryId = categoryId._id
+    }
+
+    // пагинация
+    const page = Number(req.query.page) || 1
+    const count = await Post.count(findParams)
+    const pagination = getPagination(page, count)
+    pagination.root = "/"
+    if (categoryId) pagination.root += categorySlug
+
+
+    //посты
+    const posts = await Post.find(findParams).lean().skip(pagination.skip).limit(config.maxPerPage)
+    await (() => {
+      return new Promise(async resolve => {
+        let postsCount = posts.length
+        posts.forEach(async post => {
+          post.category = categories.find(cat => cat._id == post.categoryId)
+          post.commentsCount = await Comment.count({ postId: post._id })
+          if (--postsCount <= 0) resolve()
+        })
+      })
+    })()
+
+    res.render("index", {
+      posts,
+      pagination,
+      categories: { list: categories, root: "/category/" },
+    })
   })
 }
